b68c6817e87def320afb5fd12b296952bcfd83b9
James Johnson <necromancer@thesorcererskeep.com>
2019-02-02 11:42:17 -0500

    Bootstrap Lua

--- src/sword.c
+++ src/sword.c
@@ -8,6 +8,12 @@
 #include "swordlib.h"
 
 int main(int argv, char* argc[]) {
-  printf("Hello, Swords & Sorcery!\n");
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+  int err = luaL_loadfile(L, "./scripts/main.lua") || lua_pcall(L, 0, 0, 0);
+  if (err) {
+     l_error(L, "%s\n", lua_tostring(L, -1));
+  }
+  lua_close(L);
   return EXIT_SUCCESS;
 }
