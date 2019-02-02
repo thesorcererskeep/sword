5e7a1de3e0c423ea842dec88e112d3531f670318
James Johnson <necromancer@thesorcererskeep.com>
2019-02-02 11:12:58 -0500

    Include Lua and Sword libraries

--- src/sword.c
+++ src/sword.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+#include "swordlib.h"
+
 int main(int argv, char* argc[]) {
   printf("Hello, Swords & Sorcery!\n");
   return EXIT_SUCCESS;
