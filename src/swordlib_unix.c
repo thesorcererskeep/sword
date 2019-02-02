6b2b703afe090c25f46e8d68c3d2eeec78524a42
James Johnson <necromancer@thesorcererskeep.com>
2019-02-02 11:34:01 -0500

    Test readline

--- src/swordlib_unix.c
+++ src/swordlib_unix.c
@@ -1,3 +1,5 @@
+#include <readline/readline.h>
+
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
@@ -13,5 +15,11 @@ int con_print(lua_State *L) {
  * Reads a line of text from stdin
  */
 int con_read_line(lua_State *L) {
+  while (1) {
+    char *buf;
+    buf = readline(">");
+    printf("%s\n", buf);
+    free(buf);
+  }
   return 0;
 }
