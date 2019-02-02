76f8c6534cede8d9630c38f1ca9f60f340d7af9d
James Johnson <necromancer@thesorcererskeep.com>
2019-02-02 11:43:49 -0500

    Remove duplicate comments

--- src/swordlib_unix.c
+++ src/swordlib_unix.c
@@ -17,16 +17,10 @@ void l_error (lua_State *L, const char *format, ...) {
   exit(EXIT_FAILURE);
 }
 
-/**
- * Prints a string to stdout.
- */
 int con_print(lua_State *L) {
   return 0;
 }
 
-/**
- * Reads a line of text from stdin
- */
 int con_read_line(lua_State *L) {
   while (1) {
     char *buf;
