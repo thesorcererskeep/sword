8e34fa831c3b9d09f50ce83d6ade0af3f813c478
James Johnson <necromancer@thesorcererskeep.com>
2019-02-05 15:13:13 -0500

    Add wait command

--- src/scripts/library.lua
+++ src/scripts/library.lua
@@ -4,10 +4,17 @@ local function do_quit(args)
   os.exit(0)
 end
 
+-- Skip a turn
+local function do_wait(args)
+  console.print("Time passes.")
+  return 1
+end
+
 -- Loads all default commands into the parser
 local function open()
   parser.add_command("quit", do_quit, {"quit", "q"})
-  parser.dump_commands()
+  parser.add_command("wait", do_wait, {"wait", "z"})
+  --parser.dump_commands()
 end
 
 local M = {
