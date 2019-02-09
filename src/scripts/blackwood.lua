24b8f1e1a0f989939c2fd5a61071a68e90932125
James Johnson <necromancer@thesorcererskeep.com>
2019-02-08 20:53:24 -0500

    Removing unnecessary code to work on parser

--- src/scripts/blackwood.lua
+++
@@ -1,7 +0,0 @@
-Room{
-  token = "town_square",
-  name = "Town Square",
-  description = "You are standing in the busy town square."
-}
-
-set_start("town_square")
--- src/scripts/sword.lua
+++ src/scripts/sword.lua
@@ -1,15 +1,7 @@
 parser = require "parser"
-library = require "library"
-world = require "world"
 
 function main()
-  library.open()
-  dofile("./data/scripts/blackwood.lua")
   while true do
-    local room = world.get_player_room()
-    console.print(room.name)
-    console.print(room.description)
-    console.print()
     s = parser.prompt()
     parser.parse(s)
   end
