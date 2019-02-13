63f7d942a577f86b43327e0d7868c7f787500f47
James Johnson <necromancer@thesorcererskeep.com>
2019-02-12 20:09:11 -0500

    Format comment

--- src/scripts/interpreter.lua
+++ src/scripts/interpreter.lua
@@ -65,8 +65,7 @@ end
 -- token - The name of the command
 -- help  - Help text to display if player types help [command]
 -- func  - The function used to implement the command
--- verb  - A list of synonymous verbs the player can type to invoke
---         the command
+-- verb  - A list of synonymous verbs the player can type to invoke the command
 local function add_command(token, help, func, synonyms)
   assert(token)
   assert(func)
