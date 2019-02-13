c761c804b098fdb30739132aa03acaa4c88928f8
James Johnson <necromancer@thesorcererskeep.com>
2019-02-13 13:09:42 -0500

    Add basic directions

---
+++ src/scripts/dictionary.lua
@@ -0,0 +1,6 @@
+-- dictionary.lua
+-- Adds all non-command related words to the interpreter's _dictionary
+
+-- Movement directions
+interpreter.add_word("north", "direction", {"n"})
+interpreter.add_word("south", "direction", {"s"})
