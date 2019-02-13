b4b6442fda27faed5e7d759bea849c0865ecff9a
James Johnson <necromancer@thesorcererskeep.com>
2019-02-13 10:03:15 -0500

    Add basic rooms, entities and the player

--- src/scripts/sword.lua
+++ src/scripts/sword.lua
@@ -2,16 +2,28 @@
 -- Loads in all of the data files and starts the main loop
 
 interpreter = require "interpreter"
+world = require "world"
 
 -- Loads in all of the game data
 function init()
   -- Load in all commands
   dofile("data/scripts/commands.lua")
+
+  Room{
+    key = "cave",
+    name = "Cave",
+    description = "You are standing in a damp cave."
+  }
+
+  Start('cave')
 end
 
 -- Runs the game
 function main()
   init()
+  local room = world.player:get_location()
+  console.print(room.name)
+  console.print(room.description)
   while true do
     interpreter.run()
   end
---
+++ src/scripts/world.lua
@@ -0,0 +1,64 @@
+-- world.lua
+-- Manages the game world
+
+local _rooms = {} -- Table of all rooms in game
+local _entities = {} -- Table of all entites in the game
+
+-- Entity encapsulates all game objects
+Entity = {}
+function Entity:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+-- Returns the room the Entity is currently located in
+function Entity:get_location()
+  local room = _rooms[self.location]
+  return room
+end
+
+local player = Entity:new() -- The player's entity
+
+-- Returns a specific room
+-- Parameters:
+-- key - The room's unique key
+-- Returns a table
+function get_room(key)
+  return _rooms[key]
+end
+
+-- Global convenience function to create a room
+-- Parameters:
+-- args - A table containing the room's definition
+--   Fields:
+--     key, string         - The room's unique identifier.
+--     name, string        - The room's displayed name
+--     description, string - The room's description.
+--     visited, boolean    - Whether or not the player has visited this
+--                           room before
+--     exits, table        - A list of exits from the room and their
+--                           connections. Example: {'east' = 'dark_cave'}
+function Room(args)
+  assert(args)
+  assert(args.key)
+  assert(args.name)
+  assert(args.description)
+  args.visited = args.visited or false
+  args.exits = args.exits or {}
+  _rooms[args.key] = args
+end
+
+-- Sets the starting room for the _player
+function Start(room)
+  assert(room)
+  player.location = room
+end
+
+local M = {
+  get_room = get_room,
+  player = player
+}
+
+return M
