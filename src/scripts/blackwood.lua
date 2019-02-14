39afbe033cd82ae75bc8294c25cab570899bc18a
James Johnson <necromancer@thesorcererskeep.com>
2019-02-14 16:18:27 -0500

    Add main avenue

--- src/scripts/blackwood.lua
+++ src/scripts/blackwood.lua
@@ -7,8 +7,8 @@ Room{
   description =
 [[You are standing in the heart of blackwood where the hustle and bustle of
 everyday activity is all around you. Just north of you the square opens up into
-Main Avenue. To the south is the town hall. There is also a chapel to the east
-and the bank is west.]],
+Main Avenue. To the south is the Town Hall. There is also a Chapel to the east
+and the Bank is west.]],
   exits = {
     north = "main_ave_south",
     south = "town_hall",
@@ -18,10 +18,18 @@ and the bank is west.]],
 }
 
 Room{
-  key = "forest",
-  name = "Forest",
-  description = "You emerge into a sunny forest. North leads back into the cave.",
-  exits = {north = "cave"}
+  key = "main_ave_south",
+  name = "Main Avenue South",
+  description =
+[[Shops line this section of Main Avenue. There is a General Store on the west
+side and a Blacksmith on the east. The avenue continues to the north. South is
+the Town Square.]],
+  exits = {
+    north = "main_ave_north",
+    south = "town_square",
+    east = "blacksmith",
+    west = "general_store"
+  }
 }
 
 Start('town_square')
